bf1c6590cd065fa7ffb21a0769df5bd99cf46371
VTEN <89058645+vtenwagm@users.noreply.github.com>
2023-06-07 18:52:15 +0700

    Update tdstk.py

--- python/tdstk.py
+++ python/tdstk.py
@@ -43,7 +43,7 @@ def logo():
 
     █░█ ▀█▀ █▀▀ █▄░█ ▀█▀ █▀█ █▀█ █░░
     ▀▄▀ ░█░ ██▄ █░▀█ ░█░ █▄█ █▄█ █▄▄
-          [Tool TDS facebook]
+           [Tool TDS Tiktok]
     ________________________________
     Admin1: Vu Hoang
     Admin2: Nguyen
